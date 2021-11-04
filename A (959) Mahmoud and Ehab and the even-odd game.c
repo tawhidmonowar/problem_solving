293ee939153c99eea137f838add6a9f65617c070
tawhidmonowar <tawhidmonowar@gmail.com>
2021-11-04 11:38:47 +0600

    04-11-2021

---
+++ A (959) Mahmoud and Ehab and the even-odd game.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,x,n;
+    scanf("%d",&n);
+
+    if(n%2==0)
+    {
+        printf("Mahmoud");
+    }else
+    {
+        printf("Ehab");
+    }
+
+    return 0;
+}
