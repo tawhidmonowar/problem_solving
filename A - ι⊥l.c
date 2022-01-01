94563568e59d845c92c9cab5bd581176f10e0b32
tawhidmonowar <tawhidmonowar@gmail.com>
2022-01-02 01:26:33 +0600

    2-1-2021

---
+++ A - ι⊥l.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c;
+    scanf("%d%d%d",&a,&b,&c);
+
+    if((b-a)==(c-b))
+    {
+        printf("YES");
+    }else
+    {
+        printf("NO");
+    }
+
+    return 0;
+}
