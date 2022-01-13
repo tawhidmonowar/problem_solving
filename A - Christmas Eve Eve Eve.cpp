2266c8499c05c57b02bac66171ccec4852c500f4
tawhidmonowar <tawhidmonowar@gmail.com>
2022-01-13 14:32:10 +0600

    13-01-2021

---
+++ A - Christmas Eve Eve Eve.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int d;
+    cin >> d;
+
+    if(d==25)
+    {
+        cout << "Christmas\n";
+    }else if(d==24)
+    {
+        cout << "Christmas Eve\n";
+    }else if(d==23)
+    {
+        cout << "Christmas Eve Eve\n";
+    }else if(d==22)
+    {
+        cout << "Christmas Eve Eve Eve\n";
+    }
+
+    return 0;
+}
