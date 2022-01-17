142f8f34ac9b0b7817de7fcd0415a04872b33b00
tawhidmonowar <tawhidmonowar@gmail.com>
2022-01-17 22:47:14 +0600

    17-01-2022

---
+++ For Loop.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i,x,y;
+    cin >> x >> y;
+
+    for(i=x; i<=y; i++)
+    {
+        if(i<10)
+        {
+            switch (i)
+            {
+            case 1:
+                cout << "one\n";
+                break;
+            case 2:
+                cout << "two\n";
+                break;
+            case 3:
+                cout << "three\n";
+                break;
+            case 4:
+                cout << "four\n";
+                break;
+            case 5:
+                cout << "five\n";
+                break;
+            case 6:
+                cout << "six\n";
+                break;
+            case 7:
+                cout << "seven\n";
+                break;
+            case 8:
+                cout << "eight\n";
+                break;
+            case 9:
+                cout << "nine\n";
+                break;
+            }
+        }else
+        {
+            if(i%2)
+            {
+                cout << "odd\n";
+            }else
+            {
+                cout << "even\n";
+            }
+        }
+    }
+}
