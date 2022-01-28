b1311e97409980172efc589b0c036add5eb86c93
tawhidmonowar <tawhidmonowar@gmail.com>
2022-01-28 22:23:31 +0600

    28-01-2022

---
+++ A - Health M Death.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int m,h;
+    cin >> m >> h;
+
+    if(h%m==0)
+    {
+        cout << "Yes" << endl;
+    }else
+    {
+        cout << "No" << endl;
+    }
+
+    return 0;
+}
