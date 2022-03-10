ef26323e395b1a04e7702d7730f0f92fbd9248ea
tawhidmonowar <tawhidmonowar@gmail.com>
2022-03-11 01:13:31 +0600

    11-03-2022

---
+++ FBC - Fill the Bucket.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int testCase;
+    cin >> testCase;
+
+    while (testCase--)
+    {
+        int x,k;
+        cin >> x >> k;
+        cout << x-k << endl;
+    }
+
+    return 0;
+}
