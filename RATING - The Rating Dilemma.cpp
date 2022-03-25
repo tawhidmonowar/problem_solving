53f15e1be47eca07524a8a284e43a17a01068ad5
tawhidmonowar <tawhidmonowar@gmail.com>
2022-03-25 20:42:29 +0600

    25-03-2022

---
+++ RATING - The Rating Dilemma.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int testCase;
+    cin >> testCase;
+
+    while(testCase--)
+    {
+        int s;
+        cin >> s;
+        cout << (s+1)*(-1) << endl;
+    }
+
+    return 0;
+}
