5139c936c47877f49f8b9a26c135fac18007ef6d
tawhidmonowar <tawhidmonowar@gmail.com>
2022-05-08 00:14:31 +0600

    08-05-2022

---
+++ A - Palindromic Number.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    string x;
+    cin >> x;
+
+    if(x[0]==x[2])
+    {
+        cout << "Yes" <<endl;
+    }
+    else
+    {
+        cout << "No" << endl;
+    }
+
+    return 0;
+}
