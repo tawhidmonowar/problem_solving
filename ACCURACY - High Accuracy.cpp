563a12d304ad11b016039f1075d8ccd0f1552e05
tawhidmonowar <tawhidmonowar@gmail.com>
2022-07-04 12:53:29 +0600

    04-07-2022

---
+++ ACCURACY - High Accuracy.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#define infinity 1000000000000000LL
+#define all(x) (x).begin(),(x).end()
+#define FastRead ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define mod(x,m) ((x%m)+m)%m;
+#define pi acos(-1)
+/*Author: TawhidMonowar*/
+using namespace std;
+typedef long long int ll;
+typedef unsigned long long int ull;
+const int MOD = (int)1e9+7;
+const int MAX = 1e6;
+
+void solve()
+{
+    float x,temp;
+    cin >> x;
+    temp = ceil(x/3);
+    cout << (temp*3)-x << endl;
+
+}
+
+int main()
+{
+    FastRead
+    int testCase;
+    cin >> testCase;
+
+    while(testCase--)
+    {
+        solve();
+    }
+
+    return 0;
+}
