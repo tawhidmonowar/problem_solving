b15aa2fa965d5ba5eae7f58468861019ea677bba
tawhidmonowar <tawhidmonowar@gmail.com>
2022-07-05 23:38:59 +0600

    05-07-2022

---
+++ BESTOFTWO - Best of Two.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#define infinity 1000000000000000LL
+#define all(x) (x).begin(),(x).end()
+#define FastRead ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define mod(x,m) ((x%m)+m)%m;
+#define pi acos(-1)
+/*Author: TawhidMonowar*/
+using namespace std;
+typedef long long int ll;
+typedef unsigned long long int ull;
+const int MOD = (int)1e9+7;
+const int MAX = 1e6;
+
+void solve()
+{
+    int x,y;
+    cin >>x>>y;
+
+    if(x>y)
+    {
+        cout << x <<endl;
+    }
+    else
+    {
+        cout << y <<endl;
+    }
+}
+
+int main()
+{
+    FastRead
+    int testCase;
+    cin >> testCase;
+
+    while(testCase--)
+    {
+        solve();
+    }
+
+    return 0;
+}
