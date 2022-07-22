4834a7fbec47a8070eac7038c3a84ec73cfd965c
tawhidmonowar <tawhidmonowar@gmail.com>
2022-07-22 12:13:55 +0600

    22-07-2022

---
+++ A - Last Two Digits.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define infinity 1000000000000000LL
+#define all(x) (x).begin(),(x).end()
+#define FastRead ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define mod(x,m) ((x%m)+m)%m;
+#define pi acos(-1)
+/*Author: TawhidMonowar*/
+using namespace std;
+typedef long long int ll;
+typedef unsigned long long int ull;
+const int MOD = (int)1e9+7;
+const int MAX = 1e6;
+float n,i,j,k,x,y,z;
+int temp=0,total=0;
+
+void solve()
+{
+   string x;
+   cin>>x;
+   cout << x[1] <<x[2];
+}
+
+int main()
+{
+    FastRead
+    solve();
+
+    return 0;
+}
