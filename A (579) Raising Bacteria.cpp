0cbd42d05b7407fcd2a6c25517b946201824176e
tawhidmonowar <tawhidmonowar@gmail.com>
2022-08-18 13:20:31 +0600

    18-08-2022

---
+++ A (579) Raising Bacteria.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define infinity 1000000000000000LL
+#define all(x) (x).begin(),(x).end()
+#define FastRead ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define mod(x,m) ((x%m)+m)%m;
+#define pi acos(-1)
+/*Author: TawhidMonowar*/
+const int MOD = (int)1e9+7;
+typedef long long int ll;
+using namespace std;
+const int MAX = 1e6;
+
+void solve()
+{
+    int x;
+    cin >> x;
+    bitset<64>res=x;
+    cout << res.count() << endl;
+}
+
+int main()
+{
+    FastRead
+    solve();
+
+    return 0;
+}
