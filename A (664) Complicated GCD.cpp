6da8a0526131f230978e1c3582c8ff35e8985dce
tawhidmonowar <tawhidmonowar@gmail.com>
2022-09-03 14:48:21 +0600

    09-03-2022

---
+++ A (664) Complicated GCD.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    string a, b;
+    cin >> a >> b;
+
+    if(a==b)
+    {
+        cout << a;
+    }
+    else
+    {
+        cout << 1;
+    }
+}
+
+int main()
+{
+    solve();
+    return 0;
+}
