a376587c8763309ff481b7a71974e6594ca281cf
tawhidmonowar <tawhidmonowar@gmail.com>
2022-09-26 22:11:33 +0600

    26-09-2022

---
+++ A (199) Hexadecimal's theorem.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class main {
+	
+	public static void main(String[] Args)
+	{
+		Scanner input = new Scanner(System.in);
+		int n = input.nextInt();
+		System.out.println(0+" "+0+" "+n);
+	}
+}
