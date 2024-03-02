41ef9ebeda45b59f5966e1088b51a0b85a74addd
Tawhid Monowar <tawhidmonowar@gmail.com>
2024-03-02 13:56:56 +0600

    Create 977. (24ms) Squares of a Sorted Array.cpp

---
+++ 977. (24ms) Squares of a Sorted Array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        vector<int> squares;
+
+        for(auto x : nums)
+        {
+            squares.push_back(x*x);
+        }
+
+        sort(squares.begin(),squares.end());
+        return squares;
+
+    }
+};
