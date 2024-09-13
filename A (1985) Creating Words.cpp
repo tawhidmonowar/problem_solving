66426fa3d69a60cbe44de08edf60275652e803e0
Tawhid Monowar <tawhidmonowar@gmail.com>
2024-09-13 17:44:50 +0600

    13-09-2024

---
+++ A (1985) Creating Words.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#define int long long int
+#define endl "\n"
+using namespace std;
+
+void solve(int test_case)
+{
+    string a,b,c;
+    cin >> a >> b;
+    swap(a[0],b[0]);
+    cout << a << " ";
+    cout << b << endl; 
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+
+    int testCase;
+    cin >> testCase;
+    int test_case = testCase;
+
+    while(testCase--)
+    {
+        solve(test_case-testCase);
+    }
+
+    return 0;
+}
