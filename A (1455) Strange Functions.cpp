ca37144d594b02b89a67b506e716bdf808141e7b
Tawhid Monowar <tawhidmonowar@gmail.com>
2024-09-24 02:54:23 +0600

    Create A (1455) Strange Functions.cpp

---
+++ A (1455) Strange Functions.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define int long long int
+#define endl "\n"
+using namespace std;
+
+void solve(int test_case)
+{
+    string str;
+    cin >> str;
+    cout << str.length() << endl;
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+
+    int testCase;
+    cin >> testCase;
+    int test_case = testCase;
+
+    while(testCase--)
+    {
+        solve(test_case-testCase);
+    }
+
+    return 0;
+}
